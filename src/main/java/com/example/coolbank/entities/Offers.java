ac0406928ea5af37315f6597f41b28ea9c3e6c0a
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-11-15 14:51:57 +0000

    the entity class needed for the offers

---
+++ src/main/java/com/example/coolbank/entities/Offers.java
@@ -0,0 +1,43 @@
+package com.example.coolbank.entities;
+
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Data
+@RequiredArgsConstructor
+@Table(name = "tbl_offer")
+public class Offers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String offer_name;
+
+    private Integer age;
+
+    private String location;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+
+    public Offers(String offer_name, Integer age, String location) {
+        this.offer_name = offer_name;
+        this.age = age;
+        this.location = location;
+
+    }
+}
