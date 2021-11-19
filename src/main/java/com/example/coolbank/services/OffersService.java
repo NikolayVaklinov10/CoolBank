56c649d1c7944d11b3401eeaa46ea361562cd347
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-11-19 15:55:59 +0000

    the implementation of the query from the repository interface

---
+++ src/main/java/com/example/coolbank/services/OffersService.java
@@ -0,0 +1,29 @@
+package com.example.coolbank.services;
+
+import com.example.coolbank.repositories.IOffersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Service
+public class OffersService {
+
+    private final IOffersRepository iOffersRepository;
+
+    @Autowired
+    public OffersService(IOffersRepository iOffersRepository) {
+        this.iOffersRepository = iOffersRepository;
+    }
+
+//    public void removeOlderThan() {
+//        Calendar cal = Calendar.getInstance();
+//        cal.add(Calendar.DATE, -1);
+//        Date oneDay = new Date(cal.getTimeInMillis());
+//            iOffersRepository.removeOlderThan(oneDay);
+//
+//    }
+
+
+}
