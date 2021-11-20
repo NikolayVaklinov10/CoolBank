6d199c9b390ec37ec1f1088e5f61b981967d196b
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-11-20 14:53:18 +0000

    adding stuff to the service class

--- src/main/java/com/example/coolbank/services/OffersService.java
+++ src/main/java/com/example/coolbank/services/OffersService.java
@@ -1,11 +1,13 @@
 package com.example.coolbank.services;
 
+import com.example.coolbank.entities.Offers;
 import com.example.coolbank.repositories.IOffersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class OffersService {
@@ -17,13 +19,19 @@ public class OffersService {
         this.iOffersRepository = iOffersRepository;
     }
 
-//    public Date removeOlderThan() {
-//        Calendar cal = Calendar.getInstance();
-//        cal.add(Calendar.DATE, -1);
-//        Date oneDay = new Date(cal.getTimeInMillis());
-//          return   iOffersRepository.removeOlderThan(oneDay);
-//
-//    }
+
+    public Optional<Offers> findById(Long id) {
+
+        return iOffersRepository.findById(id);
+    }
+
+    public Date removeOlderThan(Date createdAt) {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -1);
+        Date oneDay = new Date(cal.getTimeInMillis());
+        return   iOffersRepository.removeOlderThan(oneDay);
+    }
+
 
 
 }
